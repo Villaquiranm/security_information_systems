c12d0935021e30fcecc5ba639b92d655202d3307
Miguel Victoria <miguel-alfredo.victoria@grenoble-inp.org>
2018-11-04 17:16:41 +0100

    First functional Valgrantfile

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ cd /home/vagrant ##Move to home directory
 echo hola > pass.txt
 wget --progress=bar:force http://cyseclabs.com/pub/upstream44.c
 sudo apt-get --assume-yes install build-essential  ## gcc compiler
-sudo apt --assume-yes install aptitude
-##sudo aptitude --no-gui -y remove linux-image-4.4.0-138- generic linux-headers-4.4.0-138-generic
-##sudo aptitude --no-gui -y install linux-image-4.4.0-116-generic
-##sudo reboot
+sudo apt-get -y -o Dpkg::Options::="--force-confnew" install linux-image-4.4.0-116-generic
+sudo apt-get -y -o Dpkg::Options::="--force-confnew" remove linux-image-4.4.0-138-generic linux-headers-4.4.0-138-generic
+
+sudo reboot
