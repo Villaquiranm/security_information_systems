1a0a6c0a00f0fc5620ee06f2a97b919ab16bd494
Miguel Victoria <miguel-alfredo.victoria@grenoble-inp.org>
2018-11-04 16:03:51 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# security_information_systems
---
+++ script.sh
@@ -0,0 +1,8 @@
+cd /home/vagrant ##Move to home directory
+echo hola > pass.txt
+wget --progress=bar:force http://cyseclabs.com/pub/upstream44.c
+sudo apt-get --assume-yes install build-essential  ## gcc compiler
+sudo apt --assume-yes install aptitude
+##sudo aptitude --no-gui -y remove linux-image-4.4.0-138- generic linux-headers-4.4.0-138-generic
+##sudo aptitude --no-gui -y install linux-image-4.4.0-116-generic
+##sudo reboot
