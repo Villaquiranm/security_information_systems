3f3c4fb0b9a21451cf3746d2880c36ab7cd43c34
Miguel Victoria <miguel-alfredo.victoria@grenoble-inp.org>
2018-11-15 15:54:30 +0100

    Change script.sh to be adaptative to all kernels

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@ echo hola > pass.txt
 wget --progress=bar:force http://cyseclabs.com/pub/upstream44.c
 sudo apt-get --assume-yes install build-essential  ## gcc compiler
 sudo apt-get -y -o Dpkg::Options::="--force-confnew" install linux-image-4.4.0-116-generic
-sudo apt-get -y -o Dpkg::Options::="--force-confnew" remove linux-image-4.4.0-138-generic linux-headers-4.4.0-138-generic
-
+#sudo apt-get -y -o Dpkg::Options::="--force-confnew" remove linux-image-4.4.0-139-generic linux-headers-4.4.0-139-generic
+sudo apt-get -y -o Dpkg::Options::="--force-confnew" remove linux-image-$(uname -r) linux-headers-$(uname -r) 
 sudo reboot
