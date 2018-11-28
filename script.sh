455b19c24e8126535cfe61d67b9f4dcf7b44f828
Miguel Victoria Villaquiran <miguel-alfredo.victoria@grenoble-inp.org>
2018-11-28 18:48:27 +0100

    Fix Typo

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ sudo apt-get --assume-yes install build-essential
 # this command is complex because we need to force the package manager
 # to keep the current package configuration...
 # if not it will ask us to choose a configuration
-# that is clearly not possible while buildind the Vagrant container
+# that is clearly not possible while building the Vagrant container
 sudo apt-get -y -o Dpkg::Options::="--force-confnew" install linux-image-4.4.0-116-generic
 sudo apt-get -y -o Dpkg::Options::="--force-confnew" remove linux-image-$(uname -r) linux-headers-$(uname -r)
 # Reboot machine, this is necessary to charge the vulnerable kernel
